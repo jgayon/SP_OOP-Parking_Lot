a790eae2791e02221c61eb3676ac5ca0ee73cf65
jgayon <jgayon@uninorte.edu.co>
2022-11-16 21:44:03 +0000

    añadio

---
+++ src/Parking_Lot/Abstracto.py
@@ -0,0 +1,9 @@
+from abc import ABC
+
+class Abstracto(ABC):
+    def __init__(self,value) -> None:
+        self.value=value
+        super().__init__()
+    
+    def valor_cuenta():
+        pass
--- src/Parking_Lot/Parqueadero.py
+++ src/Parking_Lot/Parqueadero.py
@@ -11,3 +11,14 @@ class Parqueadero:
     cobro_dia= 30000
     cobro_hora=10000
     cobro_min= 5000
+
+    def cobro_tiempo():
+        a_cobrar= input('Escoga el tiempo que se quiere quedar: (min, hora o dia)\n')
+        if a_cobrar == 'min':
+            cuanto = int(input('Cunatos minutos se va a quedar?\n'))
+            costo = Parqueadero.cobro_min*cuanto
+        elif a_cobrar =='hora':
+            cuanto = int(input('Cunatas horas se va a quedar?\n'))
+            costo = Parqueadero.cobro_hora*cuanto
+        elif a_cobrar == 'dia':
+            costo = Parqueadero.cobro_dia
